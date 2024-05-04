5740b62628000312ec9d2323bd93485f7cd7734f
websitedeb <139923826+websitedeb@users.noreply.github.com>
2024-05-03 21:44:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const token = process.env.DISCORD_TOKEN;
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 const rolesToCheck = [
-  "Head Directive Administrator/ developer manager",
-  "First Man Xeodsa",
+  "Head mod",
+  "Admin",
+  "Head Directive Administrator/Head Developer",
   "Group God Spicyy",
-  "Moderator",
-  "Directive Administrator/ developer manager",
-  "Community manager"
+  "Second Man Xeodax",
+  "Third Man Acolon",
+  "Group leader",
+  "Developer admin",
 ];
 
 client.once("ready", () => {
@@ -37,13 +39,13 @@ client.once("ready", () => {
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand() || interaction.commandName !== "jail") return;
 
-  const user = interaction.options.getUser("user");
+  let user = interaction.options.getUser("user");
 
   // Get the member object of the user
-  const member = interaction.guild.members.cache.get(user.id);
+  let member = interaction.guild.members.cache.get(user.id);
 
   // Check if the user has any of the roles in rolesToCheck or has the role named "bot"
-  const hasRestrictedRole =
+  let hasRestrictedRole =
     member.roles.cache.some((role) => rolesToCheck.includes(role.name)) ||
     member.roles.cache.some((role) => role.name.toLowerCase() === "bot");
 
@@ -54,13 +56,13 @@ client.on("interactionCreate", async (interaction) => {
     return;
   }
 
-  const hasRequiredRole = interaction.member.roles.cache.some((role) =>
+  let hasRequiredRole = interaction.member.roles.cache.some((role) =>
     rolesToCheck.includes(role.name),
   );
 
   if (hasRequiredRole) {
     if (user) {
-      const targetUser = interaction.guild.members.cache.get(user.id);
+      let targetUser = interaction.guild.members.cache.get(user.id);
 
       if (targetUser) {
         let prisonerRole = interaction.guild.roles.cache.find(
@@ -74,7 +76,7 @@ client.on("interactionCreate", async (interaction) => {
         }
 
         // Remove all roles except for the 'Prisoner' role
-        const otherRoles = targetUser.roles.cache.filter(
+        let otherRoles = targetUser.roles.cache.filter(
           (role) => role.id !== prisonerRole.id,
         );
         await targetUser.roles.remove(otherRoles);
@@ -101,6 +103,72 @@ client.on("interactionCreate", async (interaction) => {
   }
 });
 
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand() || interaction.commandName !== "gayify") return;
+
+  let user = interaction.options.getUser("user");
+
+  // Get the member object of the user
+  let member = interaction.guild.members.cache.get(user.id);
+
+  // Check if the user has any of the roles in rolesToCheck or has the role named "bot"
+  let hasRestrictedRole =
+    member.roles.cache.some((role) => rolesToCheck.includes(role.name)) ||
+    member.roles.cache.some((role) => role.name.toLowerCase() === "bot");
+
+  if (hasRestrictedRole) {
+    interaction.reply(
+      "You cannot gayify a user who has one of the restricted roles or the 'bot' role.",
+    );
+    return;
+  }
+
+  let hasRequiredRole = interaction.member.roles.cache.some((role) =>
+    rolesToCheck.includes(role.name),
+  );
+
+  if (hasRequiredRole) {
+    if (user) {
+      let targetUser = interaction.guild.members.cache.get(user.id);
+
+      if (targetUser) {
+        let prisonerRole = interaction.guild.roles.cache.find(
+          (role) => role.name === "🏳️‍🌈gay",
+        );
+        if (!prisonerRole) {
+          prisonerRole = await interaction.guild.roles.create({
+            name: "🏳️‍🌈gay",
+            reason: "Role required for gayify command",
+          });
+        }
+
+        // Remove all roles except for the 'Prisoner' role
+        let otherRoles = targetUser.roles.cache.filter(
+          (role) => role.id !== prisonerRole.id,
+        );
+        await targetUser.roles.remove(otherRoles);
+
+        targetUser.roles
+          .add(prisonerRole)
+          .then(() => {
+            interaction.reply(`Successfully gay'ed ${user.tag}.`);
+          })
+          .catch((error) => {
+            console.error("Error adding role:", error);
+            interaction.reply("Failed to gay the user.");
+          });
+      } else {
+        interaction.reply("User not found.");
+      }
+    } else {
+      interaction.reply("User not provided.");
+    }
+  } else {
+    interaction.reply(
+      "You do not have the required roles to use this command.",
+    );
+  }
+});
+
 keepAlive();
 client.login(token);
-
